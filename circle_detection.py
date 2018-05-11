d3fe226eb02053ac34cdcb34fc6ccd95e927f238
Olivier Bitter <bittero@student.ethz.ch>
2018-05-11 18:47:44 +0200

    Minor changes

--- circle_detection.py
+++ circle_detection.py
@@ -1,5 +1,4 @@
 import cv2
-import numpy as np
 
 
 def detect_circle(image):
@@ -7,17 +6,14 @@ def detect_circle(image):
     img = cv2.imread(captcha_path + image, cv2.IMREAD_GRAYSCALE)
     height, width = img.shape
     square_img = cv2.resize(img, (height, height))
-
-    cimg = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
-
-    circles = cv2.HoughCircles(square_img, cv2.HOUGH_GRADIENT,
-                               2,
-                               minDist=1000000,
-                               param1=10,
-                               param2=1,
-                               minRadius=int(height * 0.25 * 0.5),
-                               maxRadius=int(height * 0.5 * 0.5))
-
+    circles = cv2.HoughCircles(square_img,
+                              cv2.HOUGH_GRADIENT,
+                              2,
+                              minDist=1000000,
+                              param1=10,
+                              param2=1, 
+                              minRadius=int(height * 0.25 * 0.5),
+                              maxRadius=int(height * 0.5 * 0.5))
     if circles is None:
         cv2.imshow('no circles', square_img)
         cv2.waitKey(0)
@@ -30,9 +26,9 @@ def detect_circle(image):
             # draw the center of the circle
             cv2.circle(square_img, (i[0], i[1]), 2, (0, 0, 255), 3)
 
-        cv2.imshow('detected circles in ' + image, square_img)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+    cv2.imshow('detected circles in ' + image, square_img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 
 detect_circle('captcha_1.jpg')
